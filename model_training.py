3cd1c851081fa2c7bc51c0453a8aa1d03276412b
zi-ju <60617797+zi-ju@users.noreply.github.com>
2025-03-11 21:16:56 -0700

    Update model_training.py

--- model_training.py
+++ model_training.py
@@ -5,9 +5,6 @@ def main():
     model = YOLO("yolov8n.pt")
     model.train(data="lego.yaml", epochs=50, batch=16, imgsz=640)
 
-    results = model.val(data="lego.yaml", imgsz=640, iou_thres=0.5)
-    print(results)
-
 
 if __name__ == "__main__":
     main()
